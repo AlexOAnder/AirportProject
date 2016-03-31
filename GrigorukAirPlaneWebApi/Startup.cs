261eb8133d1983f37ffd1f4624ba8f8c8cd9eaad
AlexOAnder <thanksforsource@gmail.com>
2016-03-31 21:03:26 +0300

    copy data in branch

---
+++ GrigorukAirPlaneWebApi/Startup.cs
@@ -0,0 +1,31 @@
+﻿using Autofac;
+using Autofac.Integration.WebApi;
+using GrigorukAirPlaneWebApi;
+using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace GrigorukAirPlaneWebApi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+         /*   var builder = new ContainerBuilder();
+            var container = builder.Build();
+            app.UseAutofacMiddleware(container);
+            ConfigureAuth(app);
+            */
+            var builder = new ContainerBuilder();
+            var config = new HttpConfiguration();
+            
+            var container = builder.Build();
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+
+            ConfigureAuth(app);
+        }
+    }
+}
